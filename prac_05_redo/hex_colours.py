a4f98ca4646c973431c284c2f91e7f71d37ab511
twice9 <jesseta.thach@my.jcu.edu.au>
2019-09-03 09:47:42 +1000

    finished hex_colours

---
+++ prac_05_redo/hex_colours.py
@@ -0,0 +1,17 @@
+COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
+                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+                "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+                "aquamarine4": "#458b74", "azure1": "#f0ffff",
+                "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
+                "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
+
+
+def main():
+    name_of_colour = input("Enter a colour name: ")
+    while name_of_colour != "":
+        print("The code for \"{}\" is {}".format(name_of_colour, COLOUR_CODES.get(name_of_colour)))
+        name_of_colour = input("Enter a colour name: ")
+
+
+main()
