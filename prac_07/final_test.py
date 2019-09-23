2e19f21ebbd85d5231c1c65b8cb2e576a68dc3e9
twice9 <jesseta.thach@my.jcu.edu.au>
2019-09-24 09:36:26 +1000

    Fixed errors in prac_06

--- prac_07/final_test.py
+++ prac_07/final_test.py
@@ -1,28 +1,23 @@
 from kivy.app import App
-
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.lang import Builder
-from kivy.properties import ListProperty
-
-Builder.load_string("""<Test>:
-    Label:
-        text:root.label_value[0]
-    Label:
-        text:root.label_value[1]""")
-
-
-class Test(BoxLayout):
-    label_value = ListProperty(["Im Nayeon", 'Yoo Jeongyeon'])
-
-    def __init__(self, **kwargs):
-        super(Test, self).__init__(**kwargs)
-        self.label_value = ["Im Nayeon", 'Yoo Jeongyeon', 'Hirai Momo']
 
 
 class TestApp(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Im Nayeon", 'Yoo Jeongyeon', 'Hirai Momo']
+
     def build(self):
         self.root = Builder.load_file('final_test.kv')
-        return Test()
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for name in self.names:
+            temp_label = Label(text=name, id=name)
+            self.root.ids.output_box.add_widget(temp_label)
 
 
 TestApp().run()
